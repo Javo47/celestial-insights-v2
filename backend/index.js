8a7206667b7066a0fcf280eaf016d459e173a072
Javo47 <64619957+Javo47@users.noreply.github.com>
2024-11-26 16:15:46 -0500

    Update index.js
    Changed port to 10000 for render.com

--- backend/index.js
+++ backend/index.js
@@ -70,6 +70,6 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001!!");
+app.listen(10000, () => {
+  console.log("Server is running on port 10000!!");
 });
