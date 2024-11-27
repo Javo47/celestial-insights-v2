168dd3d821ee4bfe3f642b8642015de21cd6fcc3
Javo47 <64619957+Javo47@users.noreply.github.com>
2024-11-26 21:17:54 -0500

    Update vite.config.js (new target)
    Trying new target in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from "vite"; // Importing the necessary function from Vite
-import react from "@vitejs/plugin-react"; // Importing the React plugin
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins: [react()], // Adds React plugin to Vite's configuration
-server: {
-port: 3001, // Ensure this port is open for local development
-proxy: {
-"/api": {
-target: "https://celestial-insights-v2-api.vercel.app/", // Target API URL
-changeOrigin: true, // Changes the origin of the request to the target URL
-rewrite: (path) => path.replace(/^\/api/, ""), // Rewrites the /api path to remove prefix
-secure: true // Ensures the use of HTTPS
-} // No semicolon here
-} // No semicolon here either
-}
-}); // End of export default
+  plugins: [react()],
+  server: {
+    //port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://celestial-insights-v2-api-git-fullstack-javonnies-projects.vercel.app/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
